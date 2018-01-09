3b236fcc007a70662100cc7f932a84bb8807bf3a
syb <syb@gixon.com>
2018-01-09 18:31:59 +0800

    -

---
+++ src/App.vue
@@ -0,0 +1,96 @@
+<template>
+  <div id="app">
+    <el-container>
+      <el-header>Header</el-header>
+      <el-container>
+        <el-menu class="el-menu-vertical"
+          default-active="1"
+          :collapse="isCollapse"
+          background-color="#545c64"
+          text-color="#fff"
+          active-text-color="#ffd04b">
+          <i class="collapse" 
+            :class="isCollapse ? 'el-icon-d-arrow-right' : 'el-icon-d-arrow-left'"
+            @click="onCollapseClick"></i>
+          <el-menu-item index="1" @click="onMenuItemClick('index')" >
+            <i class="el-icon-menu"></i>
+            <span slot="title">控制台</span>
+          </el-menu-item>
+          <el-menu-item index="2" @click="onMenuItemClick('movie')">
+            <i class="el-icon-view"></i>
+            <span slot="title">电影管理</span>
+          </el-menu-item>
+          <el-menu-item index="3" @click="onMenuItemClick('type')">
+            <i class="el-icon-setting"></i>
+            <span slot="title">类别管理</span>
+          </el-menu-item>
+        </el-menu>
+        <el-main>
+          <router-view></router-view>
+        </el-main>
+      </el-container>
+    </el-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data() {
+    return {
+      isCollapse: false
+    }
+  },
+  methods: {
+    onCollapseClick() {
+      this.isCollapse = !this.isCollapse
+    },
+    onMenuItemClick(path) {
+      this.$router.push(path)
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  @import './style/common';
+  #app {
+    height: 100%;
+    width: 100%;
+  }
+  .el-container {
+    height: 100%;
+    width: 100%;
+  }
+  .el-menu {
+    height: 100%;
+    .collapse {
+      text-align: center;
+      display: block;
+      height: 30px;
+      line-height: 30px;
+      color: #fff;
+      &:hover {
+        background-color: #434A50;
+      }
+    }
+  }
+  .el-menu-vertical:not(.el-menu--collapse) {
+    width: 12%;
+    min-height: 400px;
+  }
+  
+  .el-header {
+    background-color: #B3C0D1;
+    color: #333;
+    height: 200px;
+  }
+  
+  .el-aside {
+    background-color: #D3DCE6;
+  }
+
+  .el-main {
+    background-color: #fff;
+  }
+</style>
---
+++ src/page/movie/list.vue
@@ -0,0 +1,140 @@
+<template>
+	<div class="movie-list-container">
+	  <el-row class="top-bar">
+	  	<el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
+	    <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
+	    <el-col :span="12"><div class="grid-content bg-purple-light"></div></el-col>
+	  </el-row>
+	  <el-table
+	    :data="tableData3"
+	    border
+	    stripe>
+	    <el-table-column
+	      fixed
+	      prop="date"
+	      label="日期"
+	      width="150">
+	    </el-table-column>
+	    <el-table-column
+	      prop="name"
+	      label="姓名"
+	      width="120">
+	    </el-table-column>
+	    <el-table-column
+	      prop="province"
+	      label="省份"
+	      width="120">
+	    </el-table-column>
+	    <el-table-column
+	      prop="city"
+	      label="市区"
+	      width="120">
+	    </el-table-column>
+	    <el-table-column
+	      prop="address"
+	      label="地址"
+	      width="300">
+	    </el-table-column>
+	    <el-table-column
+	      prop="zip"
+	      label="邮编"
+	      width="120">
+	    </el-table-column>
+	    <el-table-column
+	      prop="zip"
+	      label="邮编"
+	      width="120">
+	    </el-table-column>
+	    <el-table-column
+	      prop="zip"
+	      label="邮编"
+	      width="120">
+	    </el-table-column>
+	    <el-table-column
+	      fixed="right"
+	      label="操作"
+	      width="180">
+	      <template slot-scope="scope">
+	        <el-button size="mini" type="primary">编辑</el-button>
+	        <el-button size="mini" type="danger">删除</el-button>
+	      </template>
+	    </el-table-column>
+	  </el-table>
+	  <div class="pagin-container">
+	    <el-pagination
+          @size-change="handleSizeChange"
+          @current-change="handleCurrentChange"
+          :current-page="currentPage4"
+          :page-size="15"
+          layout="jumper, prev, pager, next, total"
+          :total="400">
+        </el-pagination>
+      </div>
+	</div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+      	currentPage4: 4,
+        tableData3: [{
+          date: '2016-05-03',
+          name: '王小虎',
+          province: '上海',
+          city: '普陀区',
+          address: '上海市普陀区金沙江路 1518 弄',
+          zip: 200333
+        }, {
+          date: '2016-05-02',
+          name: '王小虎',
+          province: '上海',
+          city: '普陀区',
+          address: '上海市普陀区金沙江路 1518 弄',
+          zip: 200333
+        }]
+      }
+    },
+    methods: {
+      handleSizeChange(val) {
+        console.log(`每页 ${val} 条`);
+      },
+      handleCurrentChange(val) {
+        console.log(`当前页: ${val}`);
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+  .movie-list-container {
+  	$border-style: 1px solid #EBEEF5;
+    .top-bar {
+  	  height: 50px;
+  	  border-top: $border-style;
+  	  margin-bottom: 10px;
+  	}
+  	.el-table {
+  	  width: 100%;
+  	}
+  	.pagin-container {
+  	  height: 45px;
+  	  padding-top: 15px;
+  	  border: $border-style;
+  	  border-top: none;
+  	  text-align: center;
+  	}
+
+  	.bg-purple {
+	    background: #d3dce6;
+	  }
+	  .bg-purple-light {
+	    background: #e5e9f2;
+	  }
+	  .grid-content {
+    border-radius: 4px;
+    min-height: 36px;
+  }
+
+  }
+</style>
